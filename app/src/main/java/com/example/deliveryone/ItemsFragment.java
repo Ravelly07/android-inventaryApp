4bb7f27a36fac7da1689aef6bf1bb81ce9933f33
Garret Ravelli De La Cruz Tadeo <garrettravelly@gmail.com>
2022-03-09 23:50:47 -0600

    busqueda funcionando, falta admin view access

--- app/src/main/java/com/example/deliveryone/ItemsFragment.java
+++ app/src/main/java/com/example/deliveryone/ItemsFragment.java
@@ -11,12 +11,15 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import androidx.appcompat.widget.SearchView;
 
 
 public class ItemsFragment extends Fragment {
 
 
-    String exampleList[] = {"Zargo", "Bonbon", "Coni", "Peggy", "Alkapone", "Layla"}; //ESTO SOLO PARA TENER UN EJEMPLO DE VISTA
+    String exampleList[] = {"Zargo", "Bonbon", "Coni", "Peggy", "Alkapone", "Layla","Napoleaon",
+            "Lilit","Coqueta","Ron","Draco","Chispita","Bala","Canela"}; //ESTO SOLO PARA TENER UN EJEMPLO DE VISTA
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,9 +33,11 @@ public class ItemsFragment extends Fragment {
         // Inflate the layout for this fragment
         View fragmentView = inflater.inflate(R.layout.fragment_items, container, false);
         ListView listView = (ListView) fragmentView.findViewById(R.id.list_item);
+        SearchView searchView = (SearchView) fragmentView.findViewById(R.id.search_bar);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,exampleList);
+        arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,exampleList);
         listView.setAdapter(arrayAdapter);
+
 
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -42,6 +47,21 @@ public class ItemsFragment extends Fragment {
                         .navigate(R.id.action_ItemsFragment_to_itemViewFragment);
             }
         });
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                ItemsFragment.this.arrayAdapter.getFilter().filter(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                ItemsFragment.this.arrayAdapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
         return fragmentView;
     }
 }
