973cd682d29680fd2d6a76f4c8e449669218585f
Garret Ravelli De La Cruz Tadeo <garrettravelly@gmail.com>
2022-05-11 05:00:30 -0500

    Admin update + cambios de fragment

--- app/src/main/java/com/example/deliveryone/AdminViewFragment.java
+++ app/src/main/java/com/example/deliveryone/AdminViewFragment.java
@@ -20,6 +20,7 @@ import com.example.deliveryone.databinding.FragmentAdminViewBinding;
 import com.example.deliveryone.databinding.FragmentFirstBinding;
 
 public class AdminViewFragment extends Fragment {
+    private FragmentAdminViewBinding binding;
     //Elementos del view
     EditText descriptionBoxAd, costoBoxAd, ivaBoxAd, codigoBoxAd;
     Button updateButton, deleteButton;
@@ -31,55 +32,45 @@ public class AdminViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_admin_view, container, false);
-
-        return view;
+        binding = FragmentAdminViewBinding.inflate(inflater,container,false);
+        return binding.getRoot();
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        //Metodo para recibir el valor del codigo de barras la lalve primaria de nuestra tabla
-        getParentFragmentManager().setFragmentResultListener("key",this, new FragmentResultListener(){
+        //Definimos los elementos del view
+        descriptionBoxAd = (EditText) getView().findViewById(R.id.descriptionBoxAd);
+        costoBoxAd = (EditText) getView().findViewById(R.id.costoBoxAd);
+        ivaBoxAd = (EditText)  getView().findViewById(R.id.ivaBoxAd);
+        codigoBoxAd = (EditText) getView().findViewById(R.id.codigoBoxAd);
+        //Obtenemos el valor del la primaryKey del item seleccionado
+        getParentFragmentManager().setFragmentResultListener("key", this, new FragmentResultListener() {
             @Override
             public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
                 //Recibimos el valor del codebar (nuestro primarykey)
                 codebar = result.getDouble("codebar");
-
-                //Definimos los elementos del view
-                descriptionBoxAd = (EditText) getView().findViewById(R.id.descriptionBoxAd);
-                costoBoxAd = (EditText) getView().findViewById(R.id.costoBoxAd);
-                ivaBoxAd = (EditText)  getView().findViewById(R.id.ivaBoxAd);
-                codigoBoxAd = (EditText) getView().findViewById(R.id.codigoBoxAd);
-
                 //LLenamos los campos
                 MainController.FillFields(descriptionBoxAd,costoBoxAd,ivaBoxAd,codigoBoxAd,getActivity(),codebar);
-
-                updateButton = getView().findViewById(R.id.updateButton);
-                updateButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        //Toast.makeText(getContext(),"BOTON UPDATE", Toast.LENGTH_SHORT ).show();
-                        //Guardamos la key;
-                        String primaryKey = codigoBoxAd.getText().toString();
-                        MainController.update(descriptionBoxAd,costoBoxAd,ivaBoxAd,codigoBoxAd,getActivity(),primaryKey);
-
-                        NavHostFragment.findNavController(AdminViewFragment.this)
-                                .navigate(R.id.action_adminViewFragment_to_ItemsFragment);
-
-                    }
-                });
-
-                deleteButton = getView().findViewById(R.id.deleteButton);
-                deleteButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Toast.makeText(getContext(),"BOTON DELETE", Toast.LENGTH_SHORT ).show();
-                    }
-                });
             }
         });
+        //Controla lo que pasa al precionar el boton update
+        binding.updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Actualizamos
+                String primaryKey = codebar.toString();
+                MainController.update(descriptionBoxAd,costoBoxAd,ivaBoxAd,codigoBoxAd,getActivity(),primaryKey);
+                //Automaticamente nos regresa a la vista anterior | No olvidemos enviar el tipo de usuario
+                Bundle bundle = new Bundle();
+                bundle.putString("userType", "Admin");
+                getParentFragmentManager().setFragmentResult("key1",bundle);
+                NavHostFragment.findNavController(AdminViewFragment.this)
+                        .navigate(R.id.action_adminViewFragment_to_ItemsFragment);
+
+            }
+        });
+
     }
 
     //Metodos-------
@@ -95,5 +86,11 @@ public class AdminViewFragment extends Fragment {
 
     }
     //---------------
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 
 }
